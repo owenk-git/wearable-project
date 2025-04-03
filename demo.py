77a752ae3ec74d0abf62edb7e5093bdf298e480c
LiumCCY <a0939080102@gmail.com>
2025-04-02 22:10:56 -0400

    demo update for external dataset

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from nn_models.models.pure_conv import CustomConv1D
 from nn_models.models.pure_lstm import CustomLSTM, rot6_to_rotmat
+from nn_models.models.pure_transformer import transformer
 from _2_optimization.utils.optimization_utils import *
 
 import torch
@@ -11,6 +12,35 @@ import pickle
 import argparse
 import os
 from os import path as osp
+from scipy.spatial.transform import Rotation as R
+
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+def rotate_imu_data(data, x_rot, y_rot, z_rot):
+    """
+    Rotates IMU data to match the paper's coordinate system.
+    
+    Parameters:
+        data (np.ndarray): shape (N, 3), columns are [x, y, z]
+
+    Returns:
+        np.ndarray: rotated data, shape (N, 3)
+    """
+    # Define rotation (you can tweak these if needed)
+    r_z = R.from_euler('z', z_rot, degrees=True)  # Clockwise 180° about Z
+    r_x = R.from_euler('x', x_rot, degrees=True)   # Clockwise 90° about X
+    r_y = R.from_euler('y', y_rot, degrees=True)    # Counter-clockwise 90° about Y
+
+    # Final rotation matrix
+    T = r_y.as_matrix() @ r_x.as_matrix() @ r_z.as_matrix()
+
+    # Apply the rotation to each row (transpose is important!)
+    rotated = data[..., :3] @ T.T
+    norm = torch.norm(rotated, p='fro', dim=-1, keepdim=True)
+    data = torch.cat([rotated, norm], dim=-1)
+    return data
+
 
 
 def butter_low(data, order=4, fc=5, fs=100):
@@ -77,6 +107,7 @@ def evaluate_result(nn_result, combined_result, gt_angle, result_fldr=None, cali
     calib_print = '(Calibrated)' if calib else '(Uncalibrated)'
     print_result('Neural Network %s  :'%calib_print, rmse_nn_result)
     print_result('Optimization %s    :'%calib_print, rmse_opt_result)
+    return rmse_nn_result, rmse_opt_result
 
 
 def run_demo(inpt_data, gyro_data, 
@@ -108,7 +139,8 @@ def run_demo(inpt_data, gyro_data,
         # Predict orientation
         ori_model.eval()
         ori_pred = ori_model(inpt_data_ori)
-        ori_pred = rot6_to_rotmat(ori_pred)
+        #print(ori_pred.shape)
+        #ori_pred = rot6_to_rotmat(ori_pred)
 
         # Un-normalize output prediction
         alpha = alpha * angle_norm_dict['y_std'] + angle_norm_dict['y_mean']
@@ -117,30 +149,36 @@ def run_demo(inpt_data, gyro_data,
         ori_pred = ori_pred.detach().cpu().double().numpy()
         
     # Get beta from optimization
-    beta = optimization_demo(ori_pred, gyro_data, joint=joint, leg=leg)
+    #beta = optimization_demo(ori_pred, gyro_data, joint=joint, leg=leg)
 
     # Get theta from alpha and beta
-    beta = (beta - beta.mean(axis=1)[:, None]) * std_ratio + alpha.mean(axis=1)[:, None]
-    theta = weight * alpha[:, start:end] + (1 - weight) * beta
+    #beta = (beta - beta.mean(axis=1)[:, None]) * std_ratio + alpha.mean(axis=1)[:, None]
+    theta = weight * alpha[:, start:end] + (1 - weight) * alpha[:, start:end]
 
     rmse_nn, rmse_opt = evaluate_result(alpha[:, start:end], theta, gt_angle[:, start:end], 
                                         result_fldr=result_fldr, calib=False)
     print('\n\n')
     rmse_nn_calib, rmse_opt_calib = evaluate_result(alpha[:, start:end], theta, gt_angle[:, start:end], 
-                                                   result_fldr=result_fldr, calib=True)
+                                                result_fldr=result_fldr, calib=True)
 
     save_to_csv(result_fldr, rmse_nn, rmse_opt, rmse_nn_calib, rmse_opt_calib)
         
         
 
-def load_custom_data(path, is_imu_data=True):
+def load_custom_data(path, is_imu_data=True, add_norm=True):
     """Load IMU data from path.
-    Assuming data type as numpy array or torch tensor, other format has not been implemented yet.
-    The size of data is Subjects X Frames X Dimension and dimension of the data can be
-    three (X, Y, Z) or four (X, Y, Z, norm)."""
+    Assumes numpy array or torch tensor. Adds norm as 4th dimension if add_norm=True.
     
+    Parameters:
+        path (str): path to .npy or .pkl file
+        is_imu_data (bool): whether to treat data as IMU format (B x T x D)
+        add_norm (bool): if True, compute and append norm as 4th dimension if D=3
+
+    Returns:
+        torch.Tensor or np.ndarray: loaded data
+    """
     if path[-3:] == "npy":
-        _data = np.load(path)
+        _data = np.load(path, allow_pickle=True).astype(float)
         _data = torch.from_numpy(_data)
     elif path[-3:] == "pkl":
         with open(path, "rb") as fopen:
@@ -152,42 +190,44 @@ def load_custom_data(path, is_imu_data=True):
                 assert isinstance(_data, torch.Tensor), err_msg
     else:
         err_msg = "Input file format {} is not supported".format(path[-3:])
-        NotImplementedError, err_msg
+        raise NotImplementedError(err_msg)
 
-    # size of imu data is batch (subjects) X length X dimension
     if len(_data.shape) == 2:
         _data = _data[None]
-    
+
     if not is_imu_data:
-        if isinstance(_data, torch.Tensor):
-            _data = _data.double().numpy()
-        return _data
-    
+        return _data.double().numpy() if isinstance(_data, torch.Tensor) else _data
+
     sz_b, sz_l, sz_d = _data.shape
     assert sz_d in [3, 4], "Dimension of imu data should be 3 or 4"
 
-    if sz_d == 3:
+    if sz_d == 3 and add_norm:
         norm = torch.norm(_data, p='fro', dim=-1, keepdim=True)
         _data = torch.cat([_data, norm], dim=-1)
-    
     return _data
 
-
-def prepare_data(root_path, leg, device, dtype):
+def prepare_data(root_path, subject, segment1, segment2, joint, device, dtype):
     
-    seg1_accel_path = osp.join(root_path, '%s_seg1_acc.npy'%leg)
-    seg2_accel_path = osp.join(root_path, '%s_seg2_acc.npy'%leg)
-    seg1_gyro_path = osp.join(root_path, '%s_seg1_gyr.npy'%leg)
-    seg2_gyro_path = osp.join(root_path, '%s_seg2_gyr.npy'%leg)
-    gt_angle_path = osp.join(root_path, '%s_mocap_ang.npy'%leg)
+    seg1_accel_path = osp.join(root_path, subject, segment1, "acc.npy")
+    seg2_accel_path = osp.join(root_path, subject, segment2, "acc.npy")
+    seg1_gyro_path = osp.join(root_path, subject, segment1, "gyr.npy")
+    seg2_gyro_path = osp.join(root_path, subject, segment2, "gyr.npy")
+    gt_angle_path = osp.join(root_path, subject, "Left"+str(joint)+"Angle", "angle.npy")
 
     # Load custom data
     seg1_accel = load_custom_data(seg1_accel_path)
     seg2_accel = load_custom_data(seg2_accel_path)
     seg1_gyro = load_custom_data(seg1_gyro_path)
     seg2_gyro = load_custom_data(seg2_gyro_path)
+    
+    # Rotate
+    seg1_accel = rotate_imu_data(seg1_accel, 0, -90, 90)
+    seg2_accel = rotate_imu_data(seg2_accel, 0, -180, -180)
+    seg1_gyro = rotate_imu_data(seg1_gyro, 0, -90, 90)
+    seg2_gyro = rotate_imu_data(seg2_gyro, 0, -180, -180)
 
-    if gt_angle_path is not "":
+    
+    if gt_angle_path != "":
         gt_angle = load_custom_data(gt_angle_path, is_imu_data=False)
         
         # Smooth Ground-truth values
@@ -200,9 +240,12 @@ def prepare_data(root_path, leg, device, dtype):
 
     inpt_data = torch.cat([seg1_accel, seg1_gyro, seg2_accel, seg2_gyro], dim=-1)
     inpt_data = inpt_data.to(device=device, dtype=dtype)
+    
+    #print(inpt_data.shape)
 
     inpt_gyro = torch.cat([seg1_gyro[:, :, :-1], seg2_gyro[:, :, :-1]], dim=-1)
     inpt_gyro = inpt_gyro.double().numpy()
+    #print(inpt_gyro.shape)
 
     return inpt_data, inpt_gyro, gt_angle
 
@@ -244,42 +287,48 @@ if __name__ == "__main__":
     result_fldr = args.result_fldr
     joint = args.joint
     activity = args.activity
-    root_path = osp.join(args.root_path, joint)
-    leg = 'Left'    # Select the direction of your target leg
+    root_path = os.path.join(args.root_path, activity)
+    subject = 'SUB01'    # Select the direction of your target leg
+    segment1 = 'LeftThigh'
+    segment2 = 'LeftShank'
     
-    inpt_data, inpt_gyro, gt_angle = prepare_data(root_path, leg, device, dtype)
+    inpt_data, inpt_gyro, gt_angle = prepare_data(root_path, subject, segment1, segment2, joint, device, dtype)
     
     angle_model_fldr = osp.join(args.angle_model_fldr, activity, joint)
     ori_model_fldr = osp.join(args.ori_model_fldr, activity, joint)
 
-
     # Load prediction model
-    for model_fldr in [angle_model_fldr, ori_model_fldr]:
-        _, model, _ = next(os.walk(model_fldr))
-        model_fldr_ = osp.join(model_fldr, model[0])
-        with open(osp.join(model_fldr_, "model_kwargs.pkl"), "rb") as fopen:
+    for model_fldr in [angle_model_fldr , ori_model_fldr]:
+        #print(model_fldr)
+        #_, model, _ = next(os.walk(model_fldr))
+        #print(model[0])
+        #model_fldr_ = osp.join(model_fldr, model[0])
+        with open(osp.join(angle_model_fldr, "model_kwargs.pkl"), "rb") as fopen:
             model_kwargs = pickle.load(fopen)
+            print(model_kwargs)
         model = globals()['CustomConv1D'](**model_kwargs) if model_kwargs["model_type"] == "CustomConv1D" \
-                                                          else globals()['CustomLSTM'](**model_kwargs)
-        state_dict = torch.load(osp.join(model_fldr_, "model.pt"))
+                                                        else globals()['transformer'](**model_kwargs)
+        state_dict = torch.load(osp.join(angle_model_fldr, "model.pt"), map_location=torch.device('cpu'))
         model.load_state_dict(state_dict)
         model.to(device=device, dtype=dtype)
 
         if model_fldr == angle_model_fldr:
             angle_model = model
-            angle_norm_dict = torch.load(osp.join(model_fldr_, "norm_dict.pt"))['params']
+            angle_norm_dict = torch.load(osp.join(angle_model_fldr, "norm_dict.pt"), map_location=torch.device('cpu'))['params']
 
         else:
             ori_model = model
-            ori_norm_dict = torch.load(osp.join(model_fldr_, "norm_dict.pt"))['params']        
+            ori_norm_dict = torch.load(osp.join(ori_model_fldr, "norm_dict.pt"), map_location=torch.device('cpu'))['params']        
 
     # Get optimization parameters (weight, std ratio)
-    with open('Data/5_Optimization/parameters.pkl', 'rb') as fopen:
-        params = pickle.load(fopen)
-    std_ratio = params['%s_%s_std'%(joint, activity)]
-    weight = params['%s_%s_weight'%(joint, activity)]
+    #with open('Data/5_Optimization/parameters.pkl', 'rb') as fopen:
+        #params = pickle.load(fopen)
+    std_ratio = 1 #params['%s_%s_std'%(joint, activity)]
+    weight = 1 #params['%s_%s_weight'%(joint, activity)]
 
     run_demo(inpt_data, inpt_gyro, angle_norm_dict, 
-             ori_norm_dict, angle_model, 
-             ori_model, weight, std_ratio, result_fldr, 
-             joint=joint, leg=leg, gt_angle=gt_angle)
+            ori_norm_dict, angle_model, 
+            ori_model, weight, std_ratio, result_fldr, 
+            joint=joint, leg=segment1, gt_angle=gt_angle)
+    
+    
